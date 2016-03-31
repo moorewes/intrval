8fcc3d4074ec28d2d9c3dba20d98b24049bb0e6f
Wesley Moore <Wes@wesleys-macbook-pro.local>
2016-03-31 14:28:01 -0700

    Initial Commit

---
+++ Interval/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Interval
+//
+//  Created by Wesley Moore on 3/31/16.
+//  Copyright © 2016 Wes Moore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
